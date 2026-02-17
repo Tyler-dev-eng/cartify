47c74822d9792545979e4b1c74a74ed7e1ea7b97
tyleravochoc <tyler@avochoc.com>
2026-02-17 18:46:41 +0200

    Small changes

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,10 +1,41 @@
+import 'package:cartify/components/my_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+          // drawer header: logo
+          DrawerHeader(
+            child: Center(
+              child: Icon(
+                Icons.shopping_bag,
+                size: 72,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          // shop tile
+          MyListTile(
+            icon: Icons.home,
+            title: 'Shop',
+            onTap: () => context.go('/shop'),
+          ),
+
+          // cart tile
+
+          // exit tile
+        ],
+      ),
+    );
   }
 }
---
+++ lib/components/my_list_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyListTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final void Function()? onTap;
+
+  const MyListTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.grey),
+        title: Text(title),
+        onTap: onTap,
+      ),
+    );
+  }
+}
